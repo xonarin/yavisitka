893045496ee11f623876301b23b90f8c68a6a3c1
Руслан <rusyafunkysketchers@gmail.com>
2023-01-10 20:43:28 +0500

    Сделал главную кнопку входа

--- src/pages/UI/UI.tsx
+++ src/pages/UI/UI.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from '../../components/Logo/Logo';
+import LinkEntry from "../../components/LinkEntry/LinkEntry";
 
 const Demo = () => {
     return (
         <>
             Сюда можно вставлять ваши готовые компоненты для демонстрации. <br />
             <Logo />
+            <LinkEntry />
         </>
     )
 }
