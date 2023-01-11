e7920c8adcb1d9c2617f7c12fe6afa9f300d0d0d
Руслан <rusyafunkysketchers@gmail.com>
2023-01-11 18:16:04 +0500

    Сделал главную страницу, хедер, футер и добавил утилиту для scss

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,19 @@
+import React, { FC, ReactNode } from "react";
+import { cn } from "../../utils/bem-css-module";
+import styles from './Container.module.scss';
+
+const cnStyles = cn(styles, 'Container');
+
+interface ContainerProps {
+    children: ReactNode
+}
+
+const Container: FC<ContainerProps> = ({ children }) => {
+    return (
+        <div className={cnStyles()}>
+            {children}
+        </div>
+    )
+}
+
+export default Container;
