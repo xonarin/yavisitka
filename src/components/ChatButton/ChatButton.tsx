202d3b09b383698b2c2fe26f16b33b4d7600c679
RomanMitaki <r.mitaki@yandex.ru>
2023-01-17 22:08:37 +0300

    Добавил логику отрисовки кнопки коммента(в зависимости от размера экрана/наведения на карточку)

--- src/components/ChatButton/ChatButton.tsx
+++ src/components/ChatButton/ChatButton.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, {FC} from "react";
 import styles from "./ChatButton.module.scss";
-import { cn } from "../../utils/bem-css-module";
+import {cn} from "../../utils/bem-css-module";
+import useWindowSize from "../../services/hooks/useWindowSize";
+
+type TProps = {
+    counter: string,
+    isShow: boolean
+}
 
 const cnStyles = cn(styles, "ChatButton");
 const btnImage = require("../../images/сhat-btn.svg");
 
-const ChatButton = () => {
-  return (
-    <div className={cnStyles()}>
-      <button type="button" name="chatButton" className={cnStyles("button")}>
-        <img className={cnStyles('buttonImg')} src={btnImage} alt="Значок кнопки чата"/>
-      </button>
-      <p className={cnStyles('commentCounter')}>2</p>
-    </div>
-  );
+const ChatButton: FC<TProps> = (props) => {
+    const smallCounter = {
+        width: '20px',
+        right: '-7px'
+    };
+    const bigCounter = {
+        width: '28px',
+        right: '-14px'
+    }
+    const size = useWindowSize();
+
+    return size.width < 768 ?
+        (<div className={cnStyles()}>
+                <button type="button" name="chatButton" className={cnStyles("button")}>
+                    <img className={cnStyles('buttonImg')} src={btnImage} alt="Значок кнопки чата"/>
+                </button>
+
+                <p className={cnStyles('commentCounter')}
+                   style={props.counter.length < 2 ? smallCounter : bigCounter}>{props.counter}</p>
+            </div>
+        ) : props.isShow ? (<div className={cnStyles()}>
+                <button type="button" name="chatButton" className={cnStyles("button")}>
+                    <img className={cnStyles('buttonImg')} src={btnImage} alt="Значок кнопки чата"/>
+                </button>
+
+                <p className={cnStyles('commentCounter')}
+                   style={props.counter.length < 2 ? smallCounter : bigCounter}>{props.counter}</p>
+            </div>
+        ) : null
 };
 
 export default ChatButton
--- src/components/ClassmateCard/ClassmateCard.tsx
+++ src/components/ClassmateCard/ClassmateCard.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from "react";
+import React, {useState, FC} from "react";
 import styles from './ClassmateCard.module.scss';
 import {cn} from '../../utils/bem-css-module';
 import {Link} from "react-router-dom";
@@ -19,9 +19,12 @@ type TProps = {
 }
 
 const ClassmateCard: FC<TProps> = (props) => {
+    const [isShown, setIsShown] = useState(false);
+
     //const photo = require('../../images/testPic.jpg');
     return (
-        <Link to={'/user'} className={cnStyles()}>
+        <Link to={'/user'} className={cnStyles()} onMouseEnter={() => setIsShown(true)}
+              onMouseLeave={() => setIsShown(false)}>
             <figure className={cnStyles('figure')}>
                 <img className={cnStyles('matePhoto')} src={props.info.url} alt="#"/>
                 <figcaption className={cnStyles('captionContainer')}>
@@ -30,7 +33,7 @@ const ClassmateCard: FC<TProps> = (props) => {
                     <p className={cnStyles('captionSecondary')}>{props.info.title}</p>
                 </figcaption>
             </figure>
-            <ChatButton/>
+            <ChatButton isShow={isShown} counter={'2'}/>
             <CommentBar/>
         </Link>
     )
