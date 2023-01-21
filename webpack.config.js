50d71d9cfc2cbe23104615769f08c0f02a0ee4d4
Руслан <rusyafunkysketchers@gmail.com>
2023-01-21 19:17:08 +0500

    Сделал инпут даты по ТЗ на 100000%

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,10 @@ module.exports = {
             {
                 test: /\.(s(a|c)ss)$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             }
 
         ],
