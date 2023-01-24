8de327f0b3dba34b976fe9fa56d39d16713eda17
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2023-01-25 00:12:32 +0500

    убрал тсигнор и почти все эни

--- src/components/Input/InputDate/InputDate.tsx
+++ src/components/Input/InputDate/InputDate.tsx
@@ -1,81 +1,73 @@
 import React, { useState } from "react";
-import DatePicker, { registerLocale, setDefaultLocale } from  "react-datepicker";
+import DatePicker, { registerLocale, setDefaultLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import getYear from "date-fns/getYear";
 import getMonth from "date-fns/getMonth";
 import addDays from "date-fns/addDays";
 import range from "lodash/range";
 import ru from "date-fns/locale/ru";
-import '../InputDate/InputDate.scss';
-
+import "../InputDate/InputDate.scss";
 
 const InputDate = () => {
-    registerLocale('ru', ru)
-    const [startDate, setStartDate] = useState(new Date());
-    const years = range(1900, getYear(new Date()) + 1, 1);
+  registerLocale("ru", ru);
+  const [startDate, setStartDate] = useState(new Date());
+  const years = range(1900, getYear(new Date()) + 1, 1);
 
-    const months = [
-        "Январь",
-        "Февраль",
-        "Март",
-        "Апрель",
-        "Май",
-        "Июнь",
-        "Июль",
-        "Август",
-        "Сентябрь",
-        "Октябрь",
-        "Ноябрь",
-        "Декабрь",
-    ];
+  const months = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ];
   return (
     <div className="react-datepicker__wrap">
-        <DatePicker
-            locale={ru}
-            maxDate={addDays(new Date(), 0)}
-            dateFormat="d.M.yyyy"
-            popperClassName="react-datepicker-popper-custom"
-            renderCustomHeader={({
-                date,
-                changeYear,
-                changeMonth,
+      <DatePicker
+        locale={ru}
+        maxDate={addDays(new Date(), 0)}
+        dateFormat="d.M.yyyy"
+        popperClassName="react-datepicker-popper-custom"
+        renderCustomHeader={({ date, changeYear, changeMonth }) => (
+          <div className="react-datepicker__wrap-select">
+            <select
+              className="react-datepicker__custom-select"
+              value={getYear(date)}
+              onChange={({ target: { value } }) => changeYear(Number(value))}
+            >
+              {years.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
 
-            }) => (
-            <div className="react-datepicker__wrap-select">
-
-                <select className="react-datepicker__custom-select"
-                    value={getYear(date)}
-                    //@ts-ignore
-                    //Todo
-                    onChange={({ target: { value } }) => changeYear(value)}
-                    >
-                        {years.map((option) => (
-                            <option key={option} value={option}>
-                            {option}
-                            </option>
-                        ))}
-                </select>
-
-                <select className="react-datepicker__custom-select"
-                    value={months[getMonth(date)]}
-                    onChange={({ target: { value } }) =>
-                        changeMonth(months.indexOf(value))
-                    }
-                >
-                {months.map((option) => (
-                    <option key={option} value={option}>
-                    {option}
-                    </option>
-                ))}
-                </select>
-            </div>
-            )}
-            selected={startDate}
-            //@ts-ignore
-            onChange={(date) => setStartDate(date)}
-        />
+            <select
+              className="react-datepicker__custom-select"
+              value={months[getMonth(date)]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+        selected={startDate}
+        onChange={(date) => (date ? setStartDate(date) : undefined)}
+      />
     </div>
   );
-  };
+};
 
 export default InputDate;
--- src/components/admin-scrollbar-container/admin-scrollbar-container.tsx
+++ src/components/admin-scrollbar-container/admin-scrollbar-container.tsx
@@ -1,11 +1,24 @@
 import styles from "./admin-scrollbar-container.module.scss";
 import { cn } from "../../utils/bem-css-module";
+import { ReactComponentElement, ReactElement } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+
+type TScrollbarContainer = {
+  negativHeightAdjustment: number;
+  children: ReactElement;
+};
 
 const cnStyles = cn(styles, "Scrollbar-container");
-//@ts-ignore
-export const ScrollbarContainer = ({ negativHeightAdjustment, children }) => {
-    let calcStyle = {
-        maxHeight: `calc(100vh - ${negativHeightAdjustment}px)`,
-      };
-  return <div style = {calcStyle} className={cnStyles()}>{children}</div>;
+export const ScrollbarContainer = ({
+  negativHeightAdjustment,
+  children,
+}: TScrollbarContainer) => {
+  let calcStyle = {
+    maxHeight: `calc(100vh - ${negativHeightAdjustment}px)`,
+  };
+  return (
+    <div style={calcStyle} className={cnStyles()}>
+      {children}
+    </div>
+  );
 };
