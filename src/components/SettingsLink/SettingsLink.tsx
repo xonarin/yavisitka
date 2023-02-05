2a5573056cabb2d7c5018d08a31c24a2dc611e75
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2023-02-05 22:44:19 +0500

    поправил общую логику

--- src/components/SettingsLink/SettingsLink.tsx
+++ src/components/SettingsLink/SettingsLink.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { block } from "bem-cn";
 import "./SettingsLink.scss";
 
 const cnStyles = block("SettingsLink");
 
 export function SettingsLink() {
+  const { pathname } = useLocation();
   return (
-    <Link to={"/test"}>
+    <Link to={pathname.endsWith("test") ? "/" : "/test"}>
       <div className={cnStyles()}></div>
     </Link>
   );
